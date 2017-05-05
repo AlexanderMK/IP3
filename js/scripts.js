e9d8799e9c846f2cdf4d3f040e3971805307572f
Alexander <mburukungu@gmail.com>
2017-05-05 15:24:20 +0300

    placed scripts

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+/* === Business Logic === */
+
+var myPing = function (number) {
+  for (var i = 1; i <= number; i++) {
+    if(i % 3 === 0 & i % 5 === 0) {
+    $("ul#output").append("<li class='remove'>pingpong</li>");
+  }   else if(i % 3 === 0){
+    $("ul#output").append("<li class='remove'>ping</li>");
+  }   else if(i % 5 ===0) {
+    $("ul#output").append("<li class='remove'>pong</li>");
+  } else {
+    $("ul#output").append("<li class='remove'>" + String(i) + "</li>")
+    }
+  }
+}
+
+/*=== User Interface ==== */
+$(document).ready(function() {
+  $("form#pingpong").submit(function(event) {
+    event.preventDefault();
+    $("li.remove").remove();
+    var number = parseInt($("input#ping").val());
+    myPing (number);
+  })
+})
